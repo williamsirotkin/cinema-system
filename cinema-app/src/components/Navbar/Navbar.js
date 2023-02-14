e076fcbaf200589c5c4953f824f83c8de14202d7
tomking14 <tomykastor@gmail.com>
2023-02-14 17:27:03 -0500

    nav bar changes

--- cinema-app/src/components/Navbar/Navbar.js
+++ cinema-app/src/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
-
+import './Navbar.css';
 function Navbar() {
   return (
-    <div>
+    <div className='navbar'>
      <Link to  = "/login"><button type="button">Login</button></Link>
      <Link to  = "/ordersummary"><button type="button">OrderSummary</button></Link>
      <Link to  = "/admin"><button type="button">Admin Homepage</button></Link>
      <Link to  = "/editprofile"><button type="button">Edit Profile</button></Link>
      <Link to  = "/addPromotions"><button type="button">Add Promotions</button></Link>
+     <Link to  = "/editprofile"><button type="button">edit profile</button></Link>
     </div>
   );
 }
