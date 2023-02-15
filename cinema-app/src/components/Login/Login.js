88e079c3e5b62b1bafa4236ab660bff11a5eaf93
marcsilverman16 <51385681+marcsilverman16@users.noreply.github.com>
2023-02-15 17:12:03 -0500

    Login Page
    Login Page

--- cinema-app/src/components/Login/Login.js
+++ cinema-app/src/components/Login/Login.js
@@ -1,12 +1,45 @@
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 
-function Login() {
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission here
+  };
+
   return (
-    <div>
-     <h1>Login</h1>
-     <Link to  = "/"><button type="button">Homepage</button></Link>
-    </div>
+    <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <div className="p-4 shadow" style={{ maxWidth: '600px', width: '100%' }}>
+        <h1 className="mb-4">Login</h1>
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" value={email} onChange={handleEmailChange} />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </Container>
   );
-}
+};
 
 export default Login;
