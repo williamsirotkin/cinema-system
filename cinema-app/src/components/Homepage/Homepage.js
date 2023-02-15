be5cd0079537e13e9b01357559a699fa8c3ebc3b
William Sirotkin <87153798+williamsirotkin@users.noreply.github.com>
2023-02-15 16:48:57 -0500

    curerent movies and coming soon

--- cinema-app/src/components/Homepage/Homepage.js
+++ cinema-app/src/components/Homepage/Homepage.js
@@ -8,31 +8,27 @@ import { Link } from 'react-router-dom'
 export default function Homepage() {
   return (
     <div class = "homepage">
-     <Carousel class = "carousel">
-        <Carousel.Item interval={1000} width = {1} height = {5}>
+    <h1> Current Movies </h1>
+    <div class = "carousel">
+     <Carousel>
+        <Carousel.Item  width = {500} height = {5}>
             <Card style={{ width: '25rem' }}>
             <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQje3-NJXiD31oJbo0LWUXA7qRM2LXRlJP50t5W_WuslLALamvbjh6CIhvxuZMC_rgVbP8&usqp=CAU" />
             <Card.Body>
                 <Card.Title>The Batman</Card.Title>
-                <Card.Text>
-                    Awesome movie description
-                </Card.Text>
                 <div class = "showtimes">
                 <Link to  = "/selectSeats"><Button variant="primary"> 2:10 </Button></Link>
-                <Link to  = "/selectSeats"><Button variant="primary"> 2:10 </Button></Link>
-                <Link to  = "/selectSeats"><Button variant="primary"> 2:10 </Button></Link>
+                <Link to  = "/selectSeats"><Button variant="primary"> 4:40 </Button></Link>
+                <Link to  = "/selectSeats"><Button variant="primary"> 7:10 </Button></Link>
                 </div>
             </Card.Body>
             </Card>
         </Carousel.Item>
-        <Carousel.Item interval={1000} width = {250}>
+        <Carousel.Item  width = {250}>
             <Card style={{ width: '25rem' }}>
             <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQje3-NJXiD31oJbo0LWUXA7qRM2LXRlJP50t5W_WuslLALamvbjh6CIhvxuZMC_rgVbP8&usqp=CAU" />
             <Card.Body>
                 <Card.Title>The Batman</Card.Title>
-                <Card.Text>
-                    Awesome movie description
-                </Card.Text>
                 <div class = "showtimes">
                 <Link to  = "/selectSeats"><Button variant="primary"> 2:10 </Button></Link>
                 <Link to  = "/selectSeats"><Button variant="primary"> 2:10 </Button></Link>
@@ -42,6 +38,38 @@ export default function Homepage() {
             </Card>
         </Carousel.Item>
 </Carousel>
+</div>
+    <h1> Coming Soon </h1>
+    <div class = "carousel">
+     <Carousel>
+        <Carousel.Item  width = {500} height = {5}>
+            <Card style={{ width: '25rem' }}>
+            <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQje3-NJXiD31oJbo0LWUXA7qRM2LXRlJP50t5W_WuslLALamvbjh6CIhvxuZMC_rgVbP8&usqp=CAU" />
+            <Card.Body>
+                <Card.Title>The Batman</Card.Title>
+                <div class = "showtimes">
+                <Link to  = "/selectSeats"><Button variant="primary"> 2:10 </Button></Link>
+                <Link to  = "/selectSeats"><Button variant="primary"> 4:40 </Button></Link>
+                <Link to  = "/selectSeats"><Button variant="primary"> 7:10 </Button></Link>
+                </div>
+            </Card.Body>
+            </Card>
+        </Carousel.Item>
+        <Carousel.Item  width = {250}>
+            <Card style={{ width: '25rem' }}>
+            <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQje3-NJXiD31oJbo0LWUXA7qRM2LXRlJP50t5W_WuslLALamvbjh6CIhvxuZMC_rgVbP8&usqp=CAU" />
+            <Card.Body>
+                <Card.Title>The Batman</Card.Title>
+                <div class = "showtimes">
+                <Link to  = "/selectSeats"><Button variant="primary"> 2:10 </Button></Link>
+                <Link to  = "/selectSeats"><Button variant="primary"> 2:10 </Button></Link>
+                <Link to  = "/selectSeats"><Button variant="primary"> 2:10 </Button></Link>
+                </div>
+            </Card.Body>
+            </Card>
+        </Carousel.Item>
+</Carousel>
+</div>
     </div>
   )
 }
