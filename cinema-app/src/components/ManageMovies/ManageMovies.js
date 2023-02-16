b2b26cd64472dfd178bbeec857558115137e521e
William Sirotkin <87153798+williamsirotkin@users.noreply.github.com>
2023-02-15 22:14:03 -0500

    manage movies page

--- cinema-app/src/components/ManageMovies/ManageMovies.js
+++ cinema-app/src/components/ManageMovies/ManageMovies.js
@@ -1,9 +1,178 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './ManageMovies.css'
 
 export default function ManageMovies() {
   return (
     <div>
+        <div className = "add-movie-row">
         <h1> Manage Movies </h1>
+        <Button variant="success"> Add Movie + </Button>
+        </div>
+        <div class = "movie-row">
+        <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+    <Card.Img variant="top" src="https://broadbandforum.co/attachments/oppenheimer-webp.6209/" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDgxOTdjMzYtZGQxMS00ZTAzLWI4Y2UtMTQzN2VlYjYyZWRiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+    <Card.Img variant="top" src="https://broadbandforum.co/attachments/oppenheimer-webp.6209/" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDgxOTdjMzYtZGQxMS00ZTAzLWI4Y2UtMTQzN2VlYjYyZWRiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    </div>
+    <div class = "movie-row">
+        <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+    <Card.Img variant="top" src="https://broadbandforum.co/attachments/oppenheimer-webp.6209/" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDgxOTdjMzYtZGQxMS00ZTAzLWI4Y2UtMTQzN2VlYjYyZWRiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit  Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+    <Card.Img variant="top" src="https://broadbandforum.co/attachments/oppenheimer-webp.6209/" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDgxOTdjMzYtZGQxMS00ZTAzLWI4Y2UtMTQzN2VlYjYyZWRiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title> The Batman </Card.Title>
+        <p> Showtimes: 2:10, 4:40</p>
+        <Button variant="primary"> Edit Movie     </Button>
+        <Button variant="warning"> Edit Showtimes </Button>
+        <Button variant="danger"> Delete Movie </Button>
+      </Card.Body>
+    </Card>
+    </div>
     </div>
   )
 }
