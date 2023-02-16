db8697d866ee0f0dd2919ace572c1ea7121a043e
tomking14 <tomykastor@gmail.com>
2023-02-16 12:28:09 -0500

    update to select movie

--- cinema-app/src/components/SelectMovie/SelectMovie.js
+++ cinema-app/src/components/SelectMovie/SelectMovie.js
@@ -1,7 +1,127 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom'
 
 export default function SelectMovie() {
   return (
-    <div><h1>SelectMovie</h1></div>
-  )
+    <div>
+    <h1 className='manageMovieTitle'> Select Movie </h1>
+    <div class = "movie-row">
+    <Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_.jpg" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+<Card.Img variant="top" src="https://broadbandforum.co/attachments/oppenheimer-webp.6209/" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDgxOTdjMzYtZGQxMS00ZTAzLWI4Y2UtMTQzN2VlYjYyZWRiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_.jpg" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+<Card.Img variant="top" src="https://broadbandforum.co/attachments/oppenheimer-webp.6209/" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDgxOTdjMzYtZGQxMS00ZTAzLWI4Y2UtMTQzN2VlYjYyZWRiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+</div>
+<div class = "movie-row">
+    <Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_.jpg" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" />
+  <Card.Body>
+  <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+<Card.Img variant="top" src="https://broadbandforum.co/attachments/oppenheimer-webp.6209/" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDgxOTdjMzYtZGQxMS00ZTAzLWI4Y2UtMTQzN2VlYjYyZWRiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_.jpg" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+<Card.Img variant="top" src="https://broadbandforum.co/attachments/oppenheimer-webp.6209/" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+<Card style={{ width: '15rem' }}>
+  <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BMDgxOTdjMzYtZGQxMS00ZTAzLWI4Y2UtMTQzN2VlYjYyZWRiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" />
+  <Card.Body>
+    <Card.Title> The Batman </Card.Title>
+    <Link to  = "/selectshowtime"><Button variant="btn btn-dark"> Select showtime</Button></Link>
+  </Card.Body>
+</Card>
+</div>
+</div>
+)
 }
