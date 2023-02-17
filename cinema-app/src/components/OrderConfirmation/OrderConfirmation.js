b820643d8fc8876cc3507d12ca5071673b04836c
tomking14 <tomykastor@gmail.com>
2023-02-16 22:09:24 -0500

    Update OrderConfirmation.js

--- cinema-app/src/components/OrderConfirmation/OrderConfirmation.js
+++ cinema-app/src/components/OrderConfirmation/OrderConfirmation.js
@@ -1,23 +1,32 @@
-import React from 'react';
-
-const OrderConfirmation = ({ movie, date, time, seats, totalCost }) => {
+import React from 'react'
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+export default function OrderConfirmation() {
   return (
-    <div>
-      <h1>Order Confirmation</h1>
-      <h2>Thank you for your purchase! Here are your order details</h2>
-      <hr/>
-      <ul >
-        <li><strong>Date:</strong> 3-10-2023</li>
-        <li><strong>Movie:</strong> Ant Man and the Wasp</li>
-        <li><strong>Time:</strong> 8:00 pm</li>
-        <li><strong>Seats:</strong> A3, A4</li>
-        <li><strong>Total Cost:</strong> $22.99</li>
+    <Container className = 'container1'>
+      <br></br>
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <h1 className='fw-bold'>ENJOY YOUR MOVIE</h1>
+          <p>You will also receive an email confirmation  with all the details</p>
+          <hr />
+          <h4>Your order details:</h4>
+          <h5><strong>Order number:</strong> #12468</h5>
+          <p><strong>Movie title:</strong> Parasite</p>
+          <p><strong>Date and time:</strong> 10:40, Friday, February 17, 2023</p>
+          <p><strong>E-Booking 4</strong> <br></br>1235 Sushi Avenue<br></br>Alpharetta,GA 30009</p>
+          <p><strong>Seats:</strong>Adult 2<br></br>Reserved Seating: H11, H12</p>
+          <hr />
+          <h2 className='fw-bold'>Payment Summary</h2>
+          <p><strong>Order total:</strong>$36.40<br></br>Charged to AmEx ending in 5623</p>
+          <Link to  = "/"><p>Refund/Exchange</p></Link>
+          <p><strong>Refundable up until 60 minutes before the screening</strong></p>
+          <hr />
 
-
-      </ul>
-    </div>
-  );
+          <Button variant="btn btn-danger" href="/">Return to homepage</Button>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
-
-export default OrderConfirmation;
 
