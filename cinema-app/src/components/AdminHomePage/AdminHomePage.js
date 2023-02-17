1d6a7779f1afdae71db39238afc6557aa2840827
William Sirotkin <87153798+williamsirotkin@users.noreply.github.com>
2023-02-16 22:23:13 -0500

    select ages page

--- cinema-app/src/components/AdminHomePage/AdminHomePage.js
+++ cinema-app/src/components/AdminHomePage/AdminHomePage.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Card, Button } from "react-bootstrap";
+import './AdminHomePage.css'
 
 export default function AdminHomePage() {
   return (
-    <div className = "admin-page">
-        <h1> Welcome William! </h1>
-        <Link to  = "/manageMovies"><Button variant="primary"> Manage Movies </Button></Link>
-        <br></br>
-        <br></br>
-        <Link to  = "/addPromotions"><Button variant="success"> Manage Promotions </Button></Link>
+    <div>
+        <h1 className = "admin-page"> Welcome William! </h1>
+        <div className = "admin-page">
+        <Link  to  = "/manageMovies"><Button variant="primary"> Manage Movies </Button></Link>
+        &nbsp; &nbsp; &nbsp;
+        <Link  to  = "/addPromotions"><Button variant="success"> Manage Promotions </Button></Link>
+        </div>
     </div>
   )
 }
