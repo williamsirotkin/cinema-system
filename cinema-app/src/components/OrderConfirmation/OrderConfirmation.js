48fff1328a25d6b7923bad18743328c96d0a07bc
marcsilverman16 <51385681+marcsilverman16@users.noreply.github.com>
2023-02-16 21:56:21 -0500

    Update OrderConfirmation.js

--- cinema-app/src/components/OrderConfirmation/OrderConfirmation.js
+++ cinema-app/src/components/OrderConfirmation/OrderConfirmation.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-export default function OrderConfirmation() {
+const OrderConfirmation = ({ movie, date, time, seats, totalCost }) => {
   return (
     <div>
-        <h1> Order Confirmation </h1>
+      <h1>Order Confirmation</h1>
+      <h2>Thank you for your purchase! Here are your order details</h2>
+      <hr/>
+      <ul >
+        <li><strong>Date:</strong> 3-10-2023</li>
+        <li><strong>Movie:</strong> Ant Man and the Wasp</li>
+        <li><strong>Time:</strong> 8:00 pm</li>
+        <li><strong>Seats:</strong> A3, A4</li>
+        <li><strong>Total Cost:</strong> $22.99</li>
+
+
+      </ul>
     </div>
-  )
+  );
 }
+
+export default OrderConfirmation;
+
