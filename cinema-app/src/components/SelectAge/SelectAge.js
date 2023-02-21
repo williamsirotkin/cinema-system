534e982330d33b090e3b0a051cfcf4cc0dcf67b9
William Sirotkin <87153798+williamsirotkin@users.noreply.github.com>
2023-02-21 10:57:51 -0500

    select ages fixed

--- cinema-app/src/components/SelectAge/SelectAge.js
+++ cinema-app/src/components/SelectAge/SelectAge.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import './SelectAge.css'
@@ -8,6 +8,22 @@ import { Link } from 'react-router-dom'
 
 //should maybe be changed to decide how many tickets per age and then choose the seats rather than choose age after choosing the seats already
 export default function SelectAge() {
+  const[childTickets, setChildTickets] = useState(0)
+  const[adultTickets, setAdultTickets] = useState(0)
+  const[seniorTickets, setSeniorTickets] = useState(0)
+
+  const handleTicketChange = (age, change) => {
+    if (age == "child") {
+      setChildTickets(Math.max(childTickets + change, 0))
+    }
+    if (age == "adult") {
+      setAdultTickets(Math.max(adultTickets + change, 0))
+    }
+    if (age == "senior") {
+      setSeniorTickets(Math.max(seniorTickets + change, 0))
+    }
+  }
+
   return (
     <div className = "select-ages-page">
       <h1 className = "select-ages">
@@ -16,38 +32,26 @@ export default function SelectAge() {
       <br></br>
       <br></br>
       <div className = "select-ages">
-      <h2 > <strong>Adult:</strong> &nbsp;  </h2>
-      <DropdownButton id="dropdown-basic-button" title="Select">
-      <Dropdown.Item> 1</Dropdown.Item>
-      <Dropdown.Item> 2</Dropdown.Item>
-      <Dropdown.Item> 3 </Dropdown.Item>
-      <Dropdown.Item> 4</Dropdown.Item>
-      <Dropdown.Item> 5 </Dropdown.Item>
-    </DropdownButton>   
+      <h2 > <strong>Adult:</strong> &nbsp;&nbsp;  </h2>
+      <Button variant="primary" onClick={() => handleTicketChange("adult", -1)}> - </Button>
+        <h2>&nbsp;{adultTickets}&nbsp;</h2>
+      <Button variant="primary" onClick={() => handleTicketChange("adult", 1)}> + </Button>
     </div>
     <br></br>
       <br></br>
       <div className = "select-ages">
-      <h2><strong>Child:</strong> &nbsp; </h2>
-      <DropdownButton id="dropdown-basic-button" title="Select">
-      <Dropdown.Item> 1</Dropdown.Item>
-      <Dropdown.Item> 2</Dropdown.Item>
-      <Dropdown.Item> 3 </Dropdown.Item>
-      <Dropdown.Item> 4 </Dropdown.Item>
-      <Dropdown.Item> 5 </Dropdown.Item>
-    </DropdownButton>   
+      <h2><strong>Child:</strong> &nbsp;&nbsp; </h2>
+      <Button variant="primary" onClick={() => handleTicketChange("child", -1)}> - </Button>
+      <h2>&nbsp;{childTickets}&nbsp;</h2>
+      <Button variant="primary" onClick={() => handleTicketChange("child", 1)}> + </Button>
     </div> 
     <br></br>
       <br></br>
       <div className = "select-ages">
       <h2><strong>Senior:</strong> &nbsp; </h2>
-      <DropdownButton id="dropdown-basic-button3" title="Select">
-      <Dropdown.Item> 1</Dropdown.Item>
-      <Dropdown.Item> 2</Dropdown.Item>
-      <Dropdown.Item> 3</Dropdown.Item>
-      <Dropdown.Item> 4 </Dropdown.Item>
-      <Dropdown.Item> 5</Dropdown.Item>
-    </DropdownButton>   
+      <Button variant="primary" onClick={() => handleTicketChange("senior", -1)}> - </Button>
+      <h2>&nbsp;{seniorTickets}&nbsp;</h2>
+      <Button variant="primary" onClick={() => handleTicketChange("senior", 1)}> + </Button>
     </div> 
     <br></br>
     <br></br>
