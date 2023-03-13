3779f22ed16969021e88452d242c61896f1868b7
William Sirotkin <87153798+williamsirotkin@users.noreply.github.com>
2023-03-13 13:29:35 -0400

    names

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 def hello_world():
-    return 'Hello, World!'
+    return 'This is our backend'
+
+@app.route('/names', methods=['POST'])
+def names():
+    data = request.json
+    name = data['name']
+    age = data['age']
+    if name == "William":
+        return "Sirotkin"
+    if name == "Tom":
+        return "Kostoryano"
 
 if __name__ == '__main__':
     app.run()
