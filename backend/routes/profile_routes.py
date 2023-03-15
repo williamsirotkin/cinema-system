0428b876222591842cd333a0d4ad6fd159bcaea5
marcsilverman16 <51385681+marcsilverman16@users.noreply.github.com>
2023-03-15 13:19:53 -0400

    added optional fields to backend

--- backend/routes/profile_routes.py
+++ backend/routes/profile_routes.py
@@ -13,12 +13,27 @@ def profile_home():
 def create_profile():
     data = request.json
 
+    billingAddress = ""
+    if (data['billing_address']):
+        billingAddress = data['billing_address']
+
+    cardInfo = ""
+    if (data['card_info']):
+        cardInfo = data['card_info']
+
+    birthDay = ""
+    if (data['birthday']):
+        birthDay = data['birthday']
+
     user = {
         'first_name' : data['first_name'],
         'email' : data['email'],
         'last_name' : data['last_name'],
         'password' : data['password'],
-        'active': True
+        'active': True,
+        'billing_address': billingAddress,
+        'card_info': cardInfo,
+        'birthday': birthDay
     }
 
     print(user)
--- cinema-app/src/components/Signup/Signup.js
+++ cinema-app/src/components/Signup/Signup.js
@@ -14,6 +14,7 @@ const RegistrationPage = (props) => {
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  
  
 
   const handleSubmit = (e) => {
