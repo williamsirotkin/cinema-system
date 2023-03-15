3001e482ba0102f65a6a7f524bd764f684de9fce
marcsilverman16 <51385681+marcsilverman16@users.noreply.github.com>
2023-03-15 13:34:03 -0400

    option sign up added to front end

--- cinema-app/src/components/Signup/Signup.js
+++ cinema-app/src/components/Signup/Signup.js
@@ -14,7 +14,10 @@ const RegistrationPage = (props) => {
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
+  const [billingAddress, setBillingAddress] = useState('');
+  const [cardInfo, setCardInfo] = useState('')
+  const [birthday, setBirthday] = useState('')
+
  
 
   const handleSubmit = (e) => {
@@ -23,11 +26,11 @@ const RegistrationPage = (props) => {
   }
 
 
-async function checkEmail(firstName, lastName, email, password) {
+async function checkEmail(firstName, lastName, email, password, billingAddress, cardInfo, birthday) {
   const check = await checkEmailInUse(email)
   if (check) {
-    createProfile(firstName, lastName, email, password); 
-    props.setUserData(firstName,lastName,email);
+    createProfile(firstName, lastName, email, password, billingAddress, cardInfo, birthday); 
+    props.setUserData(firstName,lastName,email, billingAddress, cardInfo, birthday);
     nav('/registrationConfirmationPage', {replace: true})
   } else {
     setErrorMessage("Email is already in use, please login with that email or use another email address to sign up")
@@ -80,6 +83,37 @@ async function checkEmail(firstName, lastName, email, password) {
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
+
+        <Form.Group controlId="formBasicBillingAddress">
+          <Form.Label>Billing Address</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Billing Address"
+            value={billingAddress}
+            onChange={(e) => setBillingAddress(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formCardInfo">
+          <Form.Label>Card Info</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Card Info"
+            value={cardInfo}
+            onChange={(e) => setCardInfo(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBirthday">
+          <Form.Label>Birthday (dd/mm/yy)</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Birthday"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+          />
+        </Form.Group>
+        
         <br></br>
         
        <Button variant="btn btn-danger" onClick = {() => checkEmail(firstName, lastName, email, password)} type="submit">
