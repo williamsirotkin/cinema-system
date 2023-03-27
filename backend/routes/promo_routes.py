bdf255cee020f9771fe8974f50973cea858398e7
gilad <yehudai.gilad@gmail.com>
2023-03-27 15:47:35 -0400

    adding/deleting promotions to/from database works

--- backend/routes/promo_routes.py
+++ backend/routes/promo_routes.py
@@ -11,7 +11,24 @@ promo = Blueprint("promotions", __name__, url_prefix="/promotions")
 
 @promo.route("/")
 def promo_home():
-    return "This is the promo routes."
+    return "This is the promotions routes."
 
-@promo.route("/new", methods=['POST'])
-def promo_new():
+@promo.route('/get-all', methods=['GET'])
+def get_all():
+    all_items = list(db.promotions.find())
+    for item in all_items:
+        item['_id'] = str(item['_id'])
+    return jsonify(all_items)
+
+@promo.route("/add", methods=['POST'])
+def promo_add():
+    data = request.json
+    db.promotions.insert_one(data)
+    return 'OK'
+
+@promo.route("/delete/<promoId>", methods=['DELETE'])
+def promo_delete(promoId):
+    print(promoId)
+    db.promotions.delete_one({'_id': ObjectId(promoId)})
+    return 'OK'
+
