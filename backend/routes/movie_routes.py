d34005fa36877b33bd63dbed5f4c33f727fb8bd3
Idan Tzurdecker <41137992+IdanTzurdecker@users.noreply.github.com>
2023-03-26 23:04:14 -0400

    added Get movies
    returns movies or movies with extra details if isDetails = true in query params

--- backend/routes/movie_routes.py
+++ backend/routes/movie_routes.py
@@ -2,6 +2,7 @@ from flask import Flask, Blueprint, request, Response, jsonify
 import json, os
 from db import db
 from bson import ObjectId
+from bson import json_util
 # import jwt
 
 app = Flask(__name__)
@@ -11,6 +12,39 @@ movie = Blueprint("movie", __name__, url_prefix="/movie")
 @movie.route("/")
 def movie_home():
     return "This is the movie routes."
+
+@movie.route("/getAllMovies", methods=['GET'])
+def get_all_movies():
+    isDetails = request.args.get('isDetails')
+    print(isDetails)
+
+    #query param of isDetails ise equal to the string "true"
+
+    if isDetails == "true" :
+        pipeline = [
+            {
+                "$lookup": {
+                    "from": "movie_details",
+                    "localField": "_id",
+                    "foreignField": "movie_id",
+                    "as": "movie_detail"
+                }
+            },
+            {
+                "$addFields": {
+                    "movie_detail": {"$arrayElemAt": ["$movie_detail", 0]}
+                }
+            }
+        ]
+        movie_collection_result = db.movie.aggregate(pipeline)
+    else:
+        movie_collection_result = db.movie.find()
+
+    movie_collection = list(movie_collection_result)
+    json_result = json_util.dumps(movie_collection)
+    return json_result
+
+
 
 @movie.route("/addMovie", methods = ['POST'])
 def add_movie():
@@ -34,27 +68,40 @@ def add_movie():
     if db.movie.find_one(query_title, {'_id': 1}):
         return Response(status=409)
 
-
-    movie_map = {
+    try:
+        movie_map = {
             "title": data['title'],
             "MPAA_rating": data['MPAA_rating'],
             "photo_link": data['photo_link'],
             "trailer_link": data['trailer_link'],
+            "isShowing": data['isShowing'],
             "category": data['category']
         }
+    except KeyError as e:
+        missing_key = str(e).strip("'")
+        error_msg = f"Missing value for '{missing_key}' key in movie_map"
+        print(error_msg)
+        return Response(status=400)
 
     result1 = db.movie.insert_one(movie_map)
     movie_id = result1.inserted_id
     movie_object_id = ObjectId(movie_id)
 
-    movie_details_map = {
-        "movie_id": movie_object_id,
-        "cast": data['cast'],
-        "director": data['director'],
-        "producer": data['producer'],
-        "synopsis": data['synopsis'],
-        "reviews": data['reviews']
+    try:
+        movie_details_map = {
+            "movie_id": movie_object_id,
+            "cast": data['cast'],
+            "director": data['director'],
+            "producer": data['producer'],
+            "synopsis": data['synopsis'],
+            "reviews": data['reviews']
          }
+    except KeyError as e:
+        missing_key = str(e).strip("'")
+        error_msg = f"Missing value for '{missing_key}' key in movie_details_map"
+        print(error_msg)
+        return Response(status=400)
+
 
     result = db.movie_details.insert_one(movie_details_map)
 
@@ -66,8 +113,12 @@ def add_movie():
 @movie.route("/removeMovie", methods = ['DELETE'])
 def remove_movie():
     data = request.json
+
+    ##########################
     # test if data.user.is admin
     # return Response(status=401) unauthorized
+    ###############################
+
 
     if 'title' in data:
         if data['title'] is None or data['title'] == "":
@@ -87,7 +138,9 @@ def remove_movie():
     result_movie_details = db.movie_details.delete_one({"movie_id": movie_id})
     result_movie = db.movie.delete_one({"_id": movie_id})
 
-    #MAKE SURE TO DELETE SCHEDUEL TOO
+    ########################
+    #MAKE SURE TO DELETE SCHEDULE TOO
+    ############################
 
     if result_movie_details.deleted_count == 1 and result_movie.deleted_count == 1:
         return Response(status=202)
