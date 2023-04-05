54d4762602af970592c6c39dc95dc8b6a34573e2
William Sirotkin <87153798+williamsirotkin@users.noreply.github.com>
2023-04-05 01:06:21 -0400

    creating my own branch, backend changes didn't work with what I had

--- backend/routes/schedule_routes.py
+++ backend/routes/schedule_routes.py
@@ -2,7 +2,7 @@ from flask import Flask, Blueprint, request, Response, jsonify
 import json, os
 from db import db
 from bson import ObjectId
-from datetime import datetime, timedelta
+from datetime import datetime
 from bson import json_util
 import re
 from pymongo.collation import Collation
@@ -19,127 +19,134 @@ def schedule_home():
 def get_room_schedule():
     # postman param of collection name only, example "room_one"
     collection_name = request.args.get('collection')
+
     
     if collection_name not in db.list_collection_names():
         return jsonify({'error': f'Collection {collection_name} not found.'}), 400
 
-    # Query the database for all showtimes for the given room during the next three weeks.
-    showtimes = db.showtimes.find({'room': room_id, 'start_time': {'$gte': current_date, '$lte': end_date}}).sort(
-        'start_time')
+    collection = db[collection_name]
+    showtimes = []
+    for doc in collection.find({}, {'_id': 0, 'showtime': 1}):
+        showtime = doc['showtime']
+        showtimes.append(showtime)
 
-    # Create an empty dictionary to store the showtimes by week.
-    showtimes_by_week = {}
-
-    # Loop through all showtimes and group them by week.
-    for showtime in showtimes:
-        # Calculate the week number of the showtime.
-        week_number = (showtime['start_time'].date() - current_date.date()).days // 7 + 1
-
-        # Get the week name for the current week number.
-        week_name = f'week_{week_number}'
-
-        # Create an empty list for the week if it doesn't exist yet.
-        if week_name not in showtimes_by_week:
-            showtimes_by_week[week_name] = []
-
-        # Add the ISO-formatted showtime to the list for the current week.
-        showtimes_by_week[week_name].append(showtime['start_time'].strftime('%Y-%m-%d %H:%M:%S'))
-
-
-    # Return the JSON object.
-    return jsonify(showtimes_by_week)
+    return jsonify({'showtimes': showtimes})
 
 
 @schedule.route('/getMovieSchedule', methods=['GET'])
-def get_movie_showtimes():
-    ###### param is movie_title
-    #### RETURNS THIS #####
-    # {
-    #     "week_35": [
-    #         "2023-09-03T00:27:00"
-    #     ],
-    #     "week_36": [
-    #         "2023-09-04T00:27:00"
-    #     ],
-    #     "week_40": [
-    #         "2023-10-03T02:15:27",
-    #         "2023-10-05T02:15:27"
-    #     ],
-    #     "week_44": [
-    #         "2023-11-03T03:15:27"
-    #     ],
-    #     "week_45": [
-    #         "2023-11-06T03:15:27"
-    #     ]
-    # }
+def get_movie_schedule():
+    movie_id = request.args.get('movie_id')
     movie_title = request.args.get('movie_title')
-    showtimes = list(db.showtimes.find({'movie_title': movie_title}))
-    if len(showtimes) == 0:
-        print("No movies or no Schedule exists for: " + movie_title + ".")
-        return Response(status=400)
 
-    showtimes_by_week = {}
+    if movie_id:
+        query = {'movie_id': ObjectId(movie_id)}
+    elif movie_title:
+        query = {'movie_title': movie_title}
+    else:
+        return jsonify({'message': 'Please provide a movie_id or movie_title'})
 
-    for showtime in showtimes:
-        week = f'week_{(showtime["start_time"].isocalendar()[1])}'
-        if week not in showtimes_by_week:
-            showtimes_by_week[week] = []
-        showtimes_by_week[week].append(showtime['start_time'].strftime('%Y-%m-%d %H:%M:%S'))
+    projection = {'schedule': 1, '_id': 0}
+    schedules = db.movie_schedule.find_one(query, projection)
 
+    if not schedules:
+        return jsonify({'message': 'Movie not found'})
 
-    return jsonify(showtimes_by_week)
-
+    return jsonify({'schedule': schedules['schedule']})
 
 @schedule.route('/scheduleMovie', methods=['POST'])
 def schedule_movie():
-    ## param example  collection=room_three
+    # param is = "room_one", "room_two"....
+    # data=
     # {
     #     "showtime": [
-    #         "2023-09-04 06:27",
-    #         "2023-09-05 02:15:27",
-    #         "2023-10-06 03:15:27"
+    #         "2023-09-03 01:00:00",
+    #         "2023-11-03 02:00:00",
+    #         "2023-12-03 03:00:00"
     #     ],
-    #     "movie_title": "Interstellar",
-    #     "movie_id": "6421dcfc33a6f676ac62ee55"
+    #     "movie_title": "Superbad",
+    #     "movie_id": "6421daad33a6f676ac62ee53"
     # }
-
     data = request.json
-    movie_title = data.get('movie_title')
-    movie_id = ObjectId(data.get('movie_id'))
+    print(data)
     showtimes = data.get('showtime')
-    room = request.args.get('collection')
 
-    seats = [True] * 25
+    for key, value in data.items():
+        if value is None or value == "":
+            return Response(status=400)
 
-    # Check if the same movie is playing in the same room at the same time
-    for showtime in showtimes:
-        start_time = datetime.fromisoformat(showtime)
-        end_time = start_time + timedelta(hours=3)
-        same_showtime = db.showtimes.find_one(
-            {'room': room, 'start_time': {'$lte': end_time}, 'end_time': {'$gte': start_time}})
-        if same_showtime:
-            return jsonify({'error': f'{movie_title} is already playing in {room} at {showtime}.'}), 400
+    collection_name = request.args.get('collection')
+    print(request.args)
+    collection = db[collection_name]
 
-    # Insert showtimes into the database
-    for showtime in showtimes:
-        start_time = datetime.fromisoformat(showtime)
-        end_time = start_time + timedelta(hours=3)
-        db.showtimes.insert_one(
-            {'room': room, 'movie_title': movie_title, 'movie_id': movie_id, 'start_time': start_time,
-             'end_time': end_time, 'seats': seats})
+    time_checks = [{'showtime': datetime.fromisoformat(showtime)} for showtime in showtimes]
+    existing_schedules = collection.find({'$or': time_checks})
 
-    return jsonify({'success': f'{movie_title} has been scheduled in {room} at {showtimes}.'}), 201
+    for schedule in existing_schedules:
+        print(f"There is already a movie scheduled at {schedule['showtime']}")
+        return Response(status=400)
+
+    try:
+        schedule_rooms = [{
+            'showtime': datetime.fromisoformat(showtime),
+            'movie_id': ObjectId(data.get('movie_id')),
+            'movie_title': data.get('movie_title'),
+            'seats_available': [True] * 25,
+        } for showtime in showtimes]
+    except KeyError as e:
+        missing_key = str(e).strip("'")
+        error_msg = f"Missing value for '{missing_key}' key in movie_map"
+        print(error_msg)
+        return Response(status=400)
+
+    room_scheduling = collection.insert_many(schedule_rooms)
+    room_scheduling_ids = room_scheduling.inserted_ids
+    room_scheduling_object_ids = [ObjectId(id) for id in room_scheduling_ids]
+
+    result_find = db.movie_schedule.find_one({'movie_id': ObjectId(data.get('movie_id'))})
+    if result_find:
+        new_schedules = [{'room_name': collection_name, 'showtime': datetime.fromisoformat(showtime)} for showtime in
+                         showtimes]
+        print("testCase1")
+        movie_master_scheduling = db.movie_schedule.update_one(
+            {'movie_id': ObjectId(data.get('movie_id'))},
+            {'$push': {'schedule': {'$each': new_schedules}}}
+        )
+        print("movies thing", movie_master_scheduling)
+    else:
+        print("testCase2")
+        try:
+            movie_master_schedule = {
+                'movie_id': ObjectId(data.get('movie_id')),
+                'movie_title': data.get('movie_title'),
+                'schedule': [{'room_name': collection_name, 'showtime': datetime.fromisoformat(showtime)} for showtime
+                             in showtimes]
+            }
+        except KeyError as e:
+            missing_key = str(e).strip("'")
+            error_msg = f"Missing value for '{missing_key}' key in movie_details_map"
+            print(error_msg)
+            return Response(status=400)
+
+        movie_master_scheduling = db.movie_schedule.insert_one(movie_master_schedule)
+
+    # return the ID of the inserted document
+    if room_scheduling is None or movie_master_scheduling is None:
+        return Response(status=400)
+    else:
+        return Response(status=201)
 
 @schedule.route('/deleteShowtime', methods=['DELETE'])
 def delete_movie_schedule():
     movie_title = request.args.get('movie_title')
-    start_time = request.args.get('showtime')
+    showtime = request.args.get('showtime')
     room_name = request.args.get('room_name')
 
-    start_time = datetime.fromisoformat(start_time)
+    t_obj = datetime.strptime(showtime, "%Y-%m-%d %H:%M:%S")
 
-    # Delete the showtime from the database
-    result = db.showtimes.delete_one({'movie_title': movie_title, 'start_time': start_time})
+    # Find the movie schedule document
+    movie_schedule = db.movie_schedule.find_one({'movie_title': movie_title})
+    if not movie_schedule:
+        return jsonify({'error': 'Movie schedule not found.'}), 404
 
     # Find the room name where the movie is playing and ensure that it matches the given room name
     room_name_found = False
@@ -151,34 +158,18 @@ def delete_movie_schedule():
     if not room_name_found:
         return jsonify({'error': 'Showtime not found for the given room name.'}), 404
 
+    # Delete the document in the timeseries collection
+    room_schedule_collection = db[room_name]
+    room_schedule = room_schedule_collection.find_one({'showtime': t_obj})
+    if not room_schedule:
+        return jsonify({'error': 'Room schedule not found.'}), 404
+    room_schedule_collection.delete_many({'showtime': t_obj})
 
-@schedule.route('/deleteMovieShowtimes', methods=['DELETE'])
-def delete_showtimes_by_title():
-    movie_title = request.args.get('movie_title')
+    # Remove the showtime from the schedule array in the movie schedule document
+    for i, schedule in enumerate(movie_schedule['schedule']):
+        if str(schedule['showtime']) == showtime and schedule['room_name'] == room_name:
+            movie_schedule['schedule'].pop(i)
+            break
+    db.movie_schedule.update_one({'_id': movie_schedule['_id']}, {'$set': {'schedule': movie_schedule['schedule']}})
 
-    result = db.showtimes.delete_many({'movie_title': movie_title})
-
-    return jsonify({'deleted_count': result.deleted_count, 'movie_title': movie_title})
-
-# @schedule.route("/createCollection")
-# # DO NOTE DELETE DO NOT EXECUTE please UwU UwU
-# def schedule_collection():
-#     db.create_collection("room_five", timeseries={
-#         "timeField": "showtime",
-#         "metaField": "roomData",
-#         "granularity": "hours"
-#     })
-#
-#     return "Success"
-
-# @schedule.route("/cleanCollection")
-# # DO NOTE DELETE DO NOT EXECUTE please UwU UwU
-# def clean_collection():
-#     collection_name = request.args.get('collection')
-#     collection = db[collection_name]
-#     stink = db.collection.remove({})
-#     return stink
-#
-
-
-
+    return jsonify({'message': 'Showtime deleted successfully.'}), 200
