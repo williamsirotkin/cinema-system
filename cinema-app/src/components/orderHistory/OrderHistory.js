b4c3953d2128a5aa4957e233236f75c40ab18228
gilad <yehudai.gilad@gmail.com>
2023-04-24 14:22:21 -0400

    non logged-in cannot access history

--- cinema-app/src/components/orderHistory/OrderHistory.js
+++ cinema-app/src/components/orderHistory/OrderHistory.js
@@ -3,23 +3,30 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './orderHistory.css'
 import HistoryCard from './HistoryCard';
-export default function OrderHistory() {
 
+
+export default function OrderHistory(props) {
 const orders = []
 
-  return (
-    <div>
-         <Card  style={{ width: '85rem' }} className='card'>
-              <Card.Header as="h5">Order History</Card.Header>
-              <Card.Body>
-              {orders.map((movie) => (
-        <HistoryCard id={movie._id} title={movie.title} image={movie.image} 
-        description={movie.description} director={movie.director}  
-        producers={movie.producer} rating={movie.rating} trailer = {movie.trailer} cast={movie.cast} genre={movie.genre} review={movie.review}/>
-      ))}
-              </Card.Body>
-        </Card>
-    </div>
-  )
+if (props.user) {
+    return (
+      <div>
+          <Card  style={{ width: '85rem' }} className='card'>
+                <Card.Header as="h5">Order History</Card.Header>
+                <Card.Body>
+                {orders.map((movie) => (
+          <HistoryCard id={movie._id} title={movie.title} image={movie.image} 
+          description={movie.description} director={movie.director}  
+          producers={movie.producer} rating={movie.rating} trailer = {movie.trailer} cast={movie.cast} genre={movie.genre} review={movie.review}/>
+        ))}
+                </Card.Body>
+          </Card>
+      </div>
+    )
+  } else { 
+    return (
+      <h1> You are not authorized to view this page </h1>
+    )
+  }
 }
 
