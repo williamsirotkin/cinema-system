6ed8e2efe9b2b1fca4befe82e4d13fc188521379
William Sirotkin <87153798+williamsirotkin@users.noreply.github.com>
2023-04-25 21:13:48 -0400

    time fixed on order history

--- cinema-app/src/components/orderHistory/HistoryCard.js
+++ cinema-app/src/components/orderHistory/HistoryCard.js
@@ -13,7 +13,7 @@ export default function HistoryCard(props) {
       <div class="d-flex gap-2">
         <p class="fs-5">showtime:</p>
         {/* <p class="fs-5">{new Date(props.showtime.$date).toLocaleString()}</p> */}
-        <p class="fs-5">{formatShowtime(props.showtime.$date.toLocaleString())}</p>
+        <p class="fs-5">{formatShowtimeHere(props.showtime.$date.toLocaleString())}</p>
         </div>
       <div class="d-flex gap-2">
       <p class="fs-5">Seat numbers:</p>
@@ -50,5 +50,22 @@ export default function HistoryCard(props) {
     </Card.Body>
   </Card>
   )
+}
+
+function formatShowtimeHere(timestamp) {
+
+  const date = new Date(timestamp);
+  date.setMinutes(date.getMinutes() + 240);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = date.getHours();
+
+  const amPm = hours >= 12 ? 'PM' : 'AM';
+  const hours12 = hours % 12 || 12;
+
+  const result = `${month}-${day}-${year} at ${hours12}${amPm}`;
+  return result;
 }
 
